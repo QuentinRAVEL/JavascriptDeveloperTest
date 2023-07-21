36bed8684e5c4f7103d979b35bc3dfc436f9c26e
Quentin RAVEL <ravel.quentin@gmail.com>
2023-07-21 21:09:30 +0200

    Ajout méthodes génériques

--- test/services.test.js
+++ test/services.test.js
@@ -1,8 +1,8 @@
 assert = require('assert')
 
 const {
-  reduceAndFilterByPatternAnimalName,
-  countPeopleAndAnimals
+  reduceByPatternAndKeyProperty,
+  countArrayChildren
 } = require('../src/services/services.js')
 
 // Données de test de base
@@ -92,69 +92,90 @@ const dataTestCount = [
 ]
 
 /*
- * Tests reduceAndFilterByPatternAnimalName
+ * Tests reduceByPatternAndKeyProperty
  */
-describe('Premier test de la fonction reduceAndFilterByPatternAnimalName', function () {
+describe('Premier test de la fonction reduceByPatternAndKeyProperty', function () {
   describe("Test avec passage en premier paramétre d'un objet vide", function () {
     it('Cela doit retouner un tableau vide', function () {
-      var result = reduceAndFilterByPatternAnimalName([], '')
+      var result = reduceByPatternAndKeyProperty([], 'animals.name', '')
       assert.equal(0, result.length)
     })
   })
 })
 
-describe('Deuxième test de la fonction reduceAndFilterByPatternAnimalName', function () {
+describe('Deuxième test de la fonction reduceByPatternAndKeyProperty', function () {
   describe("Test avec passage en second paramétre d'une chaine vide", function () {
-    it("Cela doit retouner l'équivalent du tableau d'origine en entier (non filtré)", function () {
-      var result = reduceAndFilterByPatternAnimalName(dataTest, '')
-      assert.deepStrictEqual(dataTest, result)
-    })
-  })
-})
-
-describe('Troisième test de la fonction reduceAndFilterByPatternAnimalName', function () {
-  describe("Test avec passage en second paramétre d'une chaine contenue dans tous les noms d'animaux", function () {
-    it("Cela doit retouner l'équivalent du tableau d'origine en entier", function () {
-      var result = reduceAndFilterByPatternAnimalName(dataTest, 'animal')
-      assert.deepStrictEqual(dataTest, result)
-    })
-  })
-})
-
-describe('Quatrième test de la fonction reduceAndFilterByPatternAnimalName', function () {
-  describe("Test avec passage en second paramétre d'une chaine contenue dans aucun nom d'animal", function () {
     it('Cela doit retouner un tableau vide', function () {
-      var result = reduceAndFilterByPatternAnimalName(dataTest, 'TEST')
+      var result = reduceByPatternAndKeyProperty(dataTest, '', '')
       assert.equal(0, result.length)
     })
   })
 })
 
-describe('Cinquième test de la fonction reduceAndFilterByPatternAnimalName', function () {
-  describe("Test avec passage en seconde paramétre d'une chaine vide", function () {
+describe('Troisième test de la fonction reduceByPatternAndKeyProperty', function () {
+  describe("Test avec passage en troisiéme paramétre d'une chaine vide", function () {
+    it("Cela doit retouner l'équivalent du tableau d'origine en entier (non filtré)", function () {
+      var result = reduceByPatternAndKeyProperty(dataTest, 'animals.name', '')
+      assert.deepStrictEqual(dataTest, result)
+    })
+  })
+})
+
+describe('Quatrième test de la fonction reduceByPatternAndKeyProperty', function () {
+  describe("Test avec passage en troisiéme paramétre d'une chaine contenue dans tous les noms d'animaux", function () {
+    it("Cela doit retouner l'équivalent du tableau d'origine en entier", function () {
+      var result = reduceByPatternAndKeyProperty(
+        dataTest,
+        'animals.name',
+        'animal'
+      )
+      assert.deepStrictEqual(dataTest, result)
+    })
+  })
+})
+
+describe('Cinquième test de la fonction reduceByPatternAndKeyProperty', function () {
+  describe("Test avec passage en troisiéme paramétre d'une chaine contenue dans aucun nom d'animal", function () {
+    it('Cela doit retouner un tableau vide', function () {
+      var result = reduceByPatternAndKeyProperty(
+        dataTest,
+        'animals.name',
+        'TEST'
+      )
+      assert.equal(0, result.length)
+    })
+  })
+})
+
+describe('Sixième test de la fonction reduceByPatternAndKeyProperty', function () {
+  describe("Test avec passage en troisiéme paramétre d'une chaine contenue dans certains noms d'animaux", function () {
     it("Cela doit retouner un tableau filtré tel qu'attendu", function () {
-      var result = reduceAndFilterByPatternAnimalName(dataTest, 'First')
+      var result = reduceByPatternAndKeyProperty(
+        dataTest,
+        'animals.name',
+        'First'
+      )
       assert.deepStrictEqual(dataTestFiltered, result)
     })
   })
 })
 
 /*
- * Tests countPeopleAndAnimals
+ * Tests countArrayChildren
  */
-describe('Premier test de la fonction countPeopleAndAnimals', function () {
+describe('Premier test de la fonction countArrayChildren', function () {
   describe("Test avec passage en premier paramétre d'un objet vide", function () {
     it('Cela doit retouner un tableau vide', function () {
-      var result = countPeopleAndAnimals([])
+      var result = countArrayChildren([])
       assert.equal(0, result.length)
     })
   })
 })
 
-describe('Second test de la fonction countPeopleAndAnimals', function () {
+describe('Second test de la fonction countArrayChildren', function () {
   describe("Test avec passage de l'objet dataTest", function () {
     it("Cela doit retouner un tableau tel qu'attendu", function () {
-      var result = countPeopleAndAnimals(dataTest)
+      var result = countArrayChildren(dataTest)
       assert.deepStrictEqual(dataTestCount, result)
     })
   })
